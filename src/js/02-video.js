d14fccd102d8c58794ca8b0671cc474ea7dbf8f4
Anastasiia Melnyk <nastaisha1988@gmail.com>
2022-10-10 23:58:13 +0300

    fixed hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -13,19 +13,7 @@ function onSaveTime({ seconds }) {
   console.log(seconds);
 }
 
-player
-  .setCurrentTime(localStorage.getItem(STORAGE_KEY))
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+const savedTime = localStorage.getItem(STORAGE_KEY);
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
